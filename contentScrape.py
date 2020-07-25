6441aa27ed0ae07d6ef43c0fe9b9fe6f48fe622e
Tuan Dau <tukauu@gmail.com>
2020-07-25 05:10:42 -0400

    Scraped Data
    - Article content is the only one you should care about
    - No metatags yet

--- contentScrape.py
+++ contentScrape.py
@@ -3,6 +3,7 @@ import numpy as np
 import bs4
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from parsel import Selector
 
 # initialize browser, --incognito for cache/cookies
@@ -13,7 +14,6 @@ option.add_argument('--incognito')
 
 #make a chrome instance and go to the starting page
 driver = webdriver.Chrome(executable_path='C:/bin/chromedriver.exe', options=option)
-driver.get("https://www.theonion.com/a-message-from-the-publisher-only-hoover-can-get-ameri-1819583226")
 
 #loop steps:
 #P. Get URL List
@@ -21,26 +21,53 @@ driver.get("https://www.theonion.com/a-message-from-the-publisher-only-hoover-ca
 #2. Get headline, content, (metadata)
 #3. Write data
 
-headlines = []
-article = []
-content = []
-paragraphs = []
+def get_content(url):
+    driver.get(url)
+    headlines = []
+    paragraphs = []
+    finalHeadline = ""
 
-#find headline
-headline = driver.find_elements_by_xpath("/html/body/div[3]/div[4]/div[2]/header/h1/a")[0]
-print(headline.text)
-headlines.append(headline.text)
+    #find headline
+    try:
+        headline = driver.find_elements_by_xpath("/html/body/div[3]/div[4]/div[2]/header/h1/a")
+        if headline:
+            print(headline[0].text)
+            finalHeadline = headline[0].text
+        else:
+            finalHeadline = ""
+    except NoSuchElementException:
+        finalHeadline = ""
 
-# dont touch this, it gets body text but not well
-content = driver.find_elements_by_class_name("sc-77igqf-0")
-for piece in content:
-    paragraphs.append(piece.text)
-print(paragraphs)
-completeContent = ' '.join(paragraphs)
-paragraphs = []
-print(completeContent)
+    # dont touch this, it gets body text but not well
+    try:
+        content = driver.find_elements_by_class_name("sc-77igqf-0")
+        for piece in content:
+            paragraphs.append(piece.text)
+        print(paragraphs)
+        completeContent = ' '.join(paragraphs)
+        print(completeContent)
+    except NoSuchElementException:
+        completeContent = ""
 
-# format and write to csv
-format = {'Headline': headlines, 'Body text': completeContent}
-df_urls = pd.DataFrame(format)
-df_urls.to_csv(r'C:\Users\Tuan\Desktop\Seasoned Onion\articleContent.csv')
+    return finalHeadline, completeContent
+
+def write_content(headlineList, contentList):
+    # format and write to csv
+    format = {'Headline': headlineList, 'Body text': contentList}
+    df_urls = pd.DataFrame(format)
+    df_urls.to_csv(r'C:\Users\Tuan\Desktop\Seasoned Onion\articleContent.csv')
+
+urlList = pd.read_csv(r'C:\Users\Tuan\Desktop\Seasoned Onion\playtest.csv')
+
+print("-----------------------")
+print(urlList)
+
+headList = []
+bodyList = []
+
+for url in urlList['0']:
+    head, body = get_content(url)
+    headList.append(head)
+    bodyList.append(body)
+
+    write_content(headList, bodyList)
